67505852e16b9a7c8085418907b6b97c16726d36
pzamorski <pzamorski@enovatis.pl>
2019-04-03 09:13:18 +0200

    poprawka

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-const Modal = ({ pokemonData, closeModalFunc }) => {
-  const renderProperty = (propertyArray, field) => {
+class Modal extends Component {
+  renderProperty = (propertyArray, field) => {
     if (!Array.isArray(propertyArray)) {
       return '-'
     }
@@ -11,66 +11,72 @@ const Modal = ({ pokemonData, closeModalFunc }) => {
     return propertyArray.map(prop => (field ? prop[field] : prop)).join(', ')
   }
 
-  return (
-    <BackgroundWrapper>
-      <Content>
-        <div className='close' onClick={e => closeModalFunc(e)} />
+  getSimilarPokemons = (hp, type, rarity) => {}
 
-        <PokemonContent>
-          <img src={pokemonData.imageUrlHiRes} alt='details-foto' />
-          <PokemonDetails>
-            <Label>Name:</Label>
-            <Value>{pokemonData.name}</Value>
-            <Label>Super Type:</Label>
-            <Value>{pokemonData.supertype}</Value>
-            <Label>Id:</Label>
-            <Value>{pokemonData.id}</Value>
-            <Label>Series:</Label>
-            <Value>{pokemonData.series}</Value>
+  render () {
+    const { pokemonData, closeModalFunc } = this.props
 
-            <Label>DETAILS</Label>
-            <SmallItem>
-              <Label small>Resistances: </Label>
-              <Value small>{renderProperty(pokemonData.resistances, 'type')}</Value>
-            </SmallItem>
-            <SmallItem>
-              <Label small>Weaknesses: </Label>
-              <Value small>{renderProperty(pokemonData.weaknesses, 'type')}</Value>
-            </SmallItem>
+    return (
+      <BackgroundWrapper>
+        <Content>
+          <div className='close' onClick={e => closeModalFunc(e)} />
 
-            <SmallItem>
-              <Label small>Types </Label>
-              <Value small>{renderProperty(pokemonData.types)}</Value>
-            </SmallItem>
-            <SmallItem>
-              <Label small>Attacks: </Label>
-              <Value small>{renderProperty(pokemonData.attacks, 'name')}</Value>
-            </SmallItem>
-            <SmallItem>
-              <Label small>Rarity: </Label>
-              <Value small>{pokemonData.rarity}</Value>
-            </SmallItem>
-            <SmallItem>
-              <Label small>National Pokedex Number: </Label>
-              <Value small>{pokemonData.nationalPokedexNumber}</Value>
-            </SmallItem>
-            <SmallItem>
-              <Label small>Healthy points: </Label>
-              <Value small>{pokemonData.hp}</Value>
-            </SmallItem>
-            <SmallItem>
-              <Label small>Set: </Label>
-              <Value small>{pokemonData.set}</Value>
-            </SmallItem>
-            <SmallItem>
-              <Label small>Evolves from: </Label>
-              <Value small>{pokemonData.evolvesFrom || '-'}</Value>
-            </SmallItem>
-          </PokemonDetails>
-        </PokemonContent>
-      </Content>
-    </BackgroundWrapper>
-  )
+          <PokemonContent>
+            <img src={pokemonData.imageUrlHiRes} alt='details-foto' />
+            <PokemonDetails>
+              <Label>Name:</Label>
+              <Value>{pokemonData.name}</Value>
+              <Label>Super Type:</Label>
+              <Value>{pokemonData.supertype}</Value>
+              <Label>Id:</Label>
+              <Value>{pokemonData.id}</Value>
+              <Label>Series:</Label>
+              <Value>{pokemonData.series}</Value>
+
+              <Label>DETAILS</Label>
+              <SmallItem>
+                <Label small>Resistances: </Label>
+                <Value small>{this.renderProperty(pokemonData.resistances, 'type')}</Value>
+              </SmallItem>
+              <SmallItem>
+                <Label small>Weaknesses: </Label>
+                <Value small>{this.renderProperty(pokemonData.weaknesses, 'type')}</Value>
+              </SmallItem>
+
+              <SmallItem>
+                <Label small>Types </Label>
+                <Value small>{this.renderProperty(pokemonData.types)}</Value>
+              </SmallItem>
+              <SmallItem>
+                <Label small>Attacks: </Label>
+                <Value small>{this.renderProperty(pokemonData.attacks, 'name')}</Value>
+              </SmallItem>
+              <SmallItem>
+                <Label small>Rarity: </Label>
+                <Value small>{pokemonData.rarity}</Value>
+              </SmallItem>
+              <SmallItem>
+                <Label small>National Pokedex Number: </Label>
+                <Value small>{pokemonData.nationalPokedexNumber}</Value>
+              </SmallItem>
+              <SmallItem>
+                <Label small>Hit points: </Label>
+                <Value small>{pokemonData.hp}</Value>
+              </SmallItem>
+              <SmallItem>
+                <Label small>Set: </Label>
+                <Value small>{pokemonData.set}</Value>
+              </SmallItem>
+              <SmallItem>
+                <Label small>Evolves from: </Label>
+                <Value small>{pokemonData.evolvesFrom || '-'}</Value>
+              </SmallItem>
+            </PokemonDetails>
+          </PokemonContent>
+        </Content>
+      </BackgroundWrapper>
+    )
+  }
 }
 
 const BackgroundWrapper = styled.div`
